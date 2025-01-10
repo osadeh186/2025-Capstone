1e6c8d63a03721bd1b1a6f6bd5daf8230ec31200
Ofek Sadeh <osadeh186@student.fuhsd.org>
2025-01-10 10:25:57 -0800

    Create FieldObject.java

---
+++ Capstone2025/src/FieldObject.java
@@ -0,0 +1,17 @@
+public class FieldObject {
+
+  double x, y;
+
+  public FieldObject(double x, double y){
+    this.x = x;
+    this.y = y;
+  }
+
+  public getX(){
+    return x;
+  }
+
+  public getY(){
+    return y;
+  }
+}
