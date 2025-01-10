8e20623df215ac837402e9093bbac501fe247a8b
Ofek Sadeh <osadeh186@student.fuhsd.org>
2025-01-10 10:51:12 -0800

    Update Tree.java

--- Capstone2025/src/Field/Tree.java
+++ Capstone2025/src/Field/Tree.java
@@ -1,11 +1,16 @@
 package Field;
 import processing.core.PApplet;
+
 public class Tree extends FieldObject{
+
+  private double TREE_HEIGHT = 100;
+  private double TREE_WIDTH = 20;
+  
   public Tree(double x, double y){
-    super(x,y);
+    super(x,y, TREE_WIDTH, TREE_HEIGHT);
   }
 
   public void draw(PApplet surface){
-    surface.rect((float)getX(), (float)getY(), 50f, 50f);
+    surface.rect((float)getX(), (float)getY(), (float)TREE_WIDTH, (flaot)TREE_HEIGHT);
   }
 }
