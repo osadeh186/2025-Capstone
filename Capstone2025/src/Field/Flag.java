e26f8b48e9e2abe5daf55c588fc6c00963bdea50
alevy446 <alevy446@student.fuhsd.org>
2025-01-17 18:48:10 -0800

    add field to flag

--- Capstone2025/src/Field/Flag.java
+++ Capstone2025/src/Field/Flag.java
@@ -4,7 +4,8 @@ import processing.core.PApplet;
 public class Flag extends FieldObject{
   
   private static final double FLAG_RAD = 15;
-  
+  private boolean onPlayerTeam; //true - on player's team, false otherwise
+
   public Flag(double x, double y){
     super(x,y, FLAG_RAD, FLAG_RAD, false);
   }
